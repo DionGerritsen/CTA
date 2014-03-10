5977c1804f11e730fc644e2bb27926f1e7546236
Dion Gerritsen <dion@stamkracht.com>
2014-03-10 12:41:08 +0100

    Styles

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/application.css"/>
+</head>
+<body>
+Test
+</body>
+</html>
